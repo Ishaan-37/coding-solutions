21d9389ea3c9ba0bcbc28fc50dcb49ffe8d1380b
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-07-03 12:54:45 +0530

    ✅ LARGESECOND - Rating 928 | Easy | C++ | Runtime: N/A | Memory: N/A | 2026-07-03

---
+++ codechef/easy/LARGESECOND---Rating-928/solution.cpp
@@ -0,0 +1,19 @@
+# Read the number of test cases
+t = int(input())
+
+# Process each test case
+for _ in range(t):
+    # Read the size of the array
+    n = int(input())
+    
+    # Read the array elements
+    arr = [int(x) for x in input().split()]
+    
+    # Find the maximum element
+    maximum = arr[0]
+    for i in range(1, n):
+        if arr[i] > maximum:
+            maximum = arr[i]
+    
+    # Print the maximum element
+    print(maximum)
