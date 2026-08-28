c27a1ea47a7256b9f5d2d0166985c3ba02c8932a
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-08-29 01:14:39 +0530

    ✅ Maximum Subarray | Medium | Python | Runtime: 92 ms (53.78%) | Memory: 20.9 MB | 2026-08-28

---
+++ leetcode/medium/0053-maximum-subarray/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        max_sum = nums[0]
+        cur_sum = nums[0]
+        for n in nums[1:]:
+            cur_sum = max( n, cur_sum + n )
+            max_sum = max( max_sum, cur_sum )
+        return max_sum
