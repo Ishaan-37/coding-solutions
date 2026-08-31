4cf196946a7ca98c2adbfd768b02f94085b54a58
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-08-31 14:03:19 +0530

    ✅ Remove All Adjacent Duplicates In String | Easy | Python | Runtime: 33 ms (91.31%) | Memory: 13.4 MB | 2026-08-31

---
+++ leetcode/easy/1047-remove-all-adjacent-duplicates-in-string/solution.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def removeDuplicates(self, s):
+        stack = []
+        for x in s:
+            if stack and stack[-1] == x:
+                stack.pop()
+            else:
+                stack.append(x)
+        return''.join(stack)
