4b2c118029769a26ced048c4b77432ac54a91007
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-09-06 15:35:46 +0530

    ✅ Palindrome Number | Easy | Python | Runtime: 0 ms | Memory: 12.3 MB | 2026-09-06

---
+++ leetcode/easy/0009-palindrome-number/solution.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        x = str(x)
+        for i in range(len(x)):
+            if x[i] != x[-i-1]:
+                return False 
+            else:
+                return True
