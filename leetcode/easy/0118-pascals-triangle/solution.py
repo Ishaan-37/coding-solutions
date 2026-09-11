222044b8c0babe1027fdf7351e24ddcf52c27adc
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-09-11 11:53:02 +0530

    ✅ Pascal's Triangle | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 12.5 MB | 2026-09-11

--- leetcode/easy/0118-pascals-triangle/solution.py
+++ leetcode/easy/0118-pascals-triangle/solution.py
@@ -1,10 +1,12 @@
 class Solution(object):
     def generate(self, numRows):
-        ans = []
+        ans = []  #Jab tumhe loop ke andar results collect karne ho
         for i in range(numRows):
             row = [1] * (i + 1)
             for j in range(1, i):
-                row[j] = ans[i - 1][j - 1] + ans[i - 1][j]
+                left = ans[i-1][j-1]
+                right = ans[i-1][j]
+                row[j] = left + right
             ans.append(row)
         return ans
         
