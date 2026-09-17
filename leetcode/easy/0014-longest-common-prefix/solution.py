551c7ed4c3de6e76ae64f1574a9c512378767685
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-09-17 21:36:49 +0530

    ✅ Longest Common Prefix | Easy | Python | Runtime: 4 ms (14.90%) | Memory: 12.5 MB | 2026-09-17

--- leetcode/easy/0014-longest-common-prefix/solution.py
+++ leetcode/easy/0014-longest-common-prefix/solution.py
@@ -4,5 +4,4 @@ class Solution(object):
             for j in range(1 , len(strs)):
                 if  i >= len(strs[j]) or strs[0][i] != strs[j][i]:
                     return strs[0][:i]
-
         return strs[0]
