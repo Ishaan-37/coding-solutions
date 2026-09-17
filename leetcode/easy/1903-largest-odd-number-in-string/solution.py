ba375a475be78385e8e96a2c2332557b2de13b46
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-09-17 13:23:47 +0530

    ✅ Largest Odd Number in String | Easy | Python | Runtime: 0 ms | Memory: 12.4 MB | 2026-09-17

--- leetcode/easy/1903-largest-odd-number-in-string/solution.py
+++ leetcode/easy/1903-largest-odd-number-in-string/solution.py
@@ -1,6 +1,7 @@
 class Solution(object):
     def largestOddNumber(self, num):
-        for i in range(len(num)-1, -1, -1): #Last index se start karke, har baar 1 step peeche jaate hue first index tak loop karta hai.
-            if int(num[i]) % 2:
-                return num[:i+1] #String ke start (index 0) se index i tak ke characters leta hai; +1 isliye kyunki slicing mein ending index include nahi hota
-        return ""
+        n = len(num)
+        for i in range(n):
+            if int(num[i]) % 2 == 1:
+                return num[i]
+            
