1ace6a7305da705cd1f881ba944edb4f576eca4d
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-09-20 11:11:39 +0530

    ✅ Reverse Linked List | Easy | Python | Runtime: 0 ms (100.00%) | Memory: 18.1 MB | 2026-09-20

--- leetcode/easy/0206-reverse-linked-list/solution.py
+++ leetcode/easy/0206-reverse-linked-list/solution.py
@@ -2,7 +2,7 @@ class Solution(object):
     def reverseList(self, head):
         if head is None or head.next is None:
             return head                               # BASE CASE
-            newHead = self.reverseList(head.next)   # RECURSIVE CALL
-            head.next.next = head                    # WORK
-            head.next = None                         # WORK
-            return newHead
+        newHead = self.reverseList(head.next)   # RECURSIVE CALL
+        head.next.next = head                    # WORK
+        head.next = None                         # WORK
+        return newHead
