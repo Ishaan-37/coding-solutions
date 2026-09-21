3b8831ec039f7770494587f1e5c377816bff7a0c
Ishaan Maurya <ishaanmaurya3@gmail.com>
2026-09-21 23:59:38 +0530

    ✅ Roman to Integer | Easy | Python | Runtime: 12 ms (36.98%) | Memory: 12.4 MB | 2026-09-21

--- leetcode/easy/0013-roman-to-integer/solution.py
+++ leetcode/easy/0013-roman-to-integer/solution.py
@@ -7,9 +7,24 @@ class Solution(object):
             "L": 50,
             "C": 100,
             "D": 500,
-            "M": 1000
-            }
+            "M": 1000,
+            "IV": 4,
+            "IX": 9,
+            "XL": 40,
+            "XC": 90,
+            "CD": 400,
+            "CM": 900
+        }
+
         total = 0
-        for i in range(len(s)):
-            total = total + freq[s[i]]
+        i = 0
+
+        while i < len(s):
+            if i + 1 < len(s) and s[i:i+2] in freq:
+                total += freq[s[i:i+2]]
+                i += 2
+            else:
+                total += freq[s[i]]
+                i += 1
+
         return total
